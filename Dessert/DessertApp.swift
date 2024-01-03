52b21cc8c259ef7e15e9684519033838d5487730
edzellcastle <davidlindsay100@icloud.com>
2024-01-03 14:25:29 -0800

    Initial Commit

---
+++ Dessert/DessertApp.swift
@@ -0,0 +1,17 @@
+//
+//  DessertApp.swift
+//  Dessert
+//
+//  Created by David Lindsay on 1/3/24.
+//
+
+import SwiftUI
+
+@main
+struct DessertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
